7d70cf2cd2208237b49b460efd2ed92da26915e5
TechnicallyMay <masonwells01@gmail.com>
2018-12-26 00:09:47 -0700

    Merged into one file

---
+++ mason/json_io.py
@@ -0,0 +1,27 @@
+from flask import Flask
+import pokebase as pb
+import json
+
+app = Flask(__name__)
+
+@app.route('/get/<name>')
+def get_pokemon(name):
+	return get_info(name)
+
+
+def get_info(name):
+    pokemon = pb.pokemon(name)
+    data = {
+        "Name" : pokemon.name.capitalize(),
+        "Type" : pokemon.types[0].type.name.capitalize(),
+        }
+    moves = [move.move.name.capitalize() for move in pokemon.moves[:4]]
+    for i, move in enumerate(moves):
+        data["Move %d" % (i + 1)] = move
+    json_data = json.dumps(data)
+
+    return json_data
+
+
+if __name__ == '__main__':
+	app.run()
