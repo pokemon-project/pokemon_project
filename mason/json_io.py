66210b79af85bd7a01be0e89b56cad80af2008da
TechnicallyMay <masonwells01@gmail.com>
2018-12-28 22:29:21 -0700

    Added Comment

--- mason/json_io.py
+++ mason/json_io.py
@@ -8,12 +8,13 @@ app = Flask(__name__)
 def get_pokemon(name):
 	return get_info(name)
 
-
+#Getting info from API
 def get_info(name):
     pokemon = pb.pokemon(name)
     data = {
         "Name" : pokemon.name.capitalize(),
         "Type" : pokemon.types[0].type.name.capitalize(),
+		"Picture" : pokemon.image()
         }
     moves = [move.move.name.capitalize() for move in pokemon.moves[:4]]
     for i, move in enumerate(moves):
