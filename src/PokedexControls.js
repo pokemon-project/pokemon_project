173edee594cadb9d4ce1ffdf0ea1d6920a7f54b2
Michael <michaelolson.1996@protonmail.com>
2018-12-28 22:28:11 -0700

    added comment

--- src/PokedexControls.js
+++ src/PokedexControls.js
@@ -2,14 +2,23 @@ import React, {Component} from 'react'
 import './app.css'
 
 class PokedexControls extends Component {
+    constructor() {
+        super()
+        this.state = {
+
+        }
+    }
 
     render() {
         let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
         let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+        // Mapping through the alphabet and numbers
+
         let letters = alphabet.map(letter => {
             return <button className = "letter-buttons buttons">{`${letter}`}</button>
         })
-
+        
         let numberButtons = numbers.map(number => {
             return <button className = "number-buttons buttons">{`${number}`}</button>
         })
